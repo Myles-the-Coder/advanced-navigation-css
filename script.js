bff5c44ed13814b095a06ce4a07f58b96a0cc986
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-07-21 15:42:30 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const menu = document.getElementById('menu-btn')
+const overlay = document.getElementById('overlay')
+const nav1 = document.getElementById('nav-1')
+const nav2 = document.getElementById('nav-2')
+const nav3 = document.getElementById('nav-3')
+const nav4 = document.getElementById('nav-4')
+const nav5 = document.getElementById('nav-5')
+const navItems = [nav1, nav2, nav3, nav4, nav5]
+
+//Functions 
+function navAnimation(direction1, direction2) {
+  navItems.forEach((nav, index) => {
+    nav.classList.replace(`slide-${direction1}-${index + 1}`, `slide-${direction2}-${index + 1}`)
+  })
+}
+
+function toggleNav() {
+  //Toggle: Menu bars Open/Closed
+  menu.classList.toggle('change')
+  //Toggle: Menu Active
+  overlay.classList.toggle('overlay-active')
+  if(overlay.classList.contains('overlay-active')) {
+    overlay.classList.replace('overlay-slide-left', 'overlay-slide-right')
+    //Animate In - Nav Items
+    navAnimation('out', 'in')
+  } else {
+    overlay.classList.replace('overlay-slide-right', 'overlay-slide-left')
+    navAnimation('in', 'out')
+  }
+}
+
+// Event Listeners
+menu.addEventListener('click', toggleNav)
+navItems.forEach(nav => {
+  nav.addEventListener('click', toggleNav)
+})
